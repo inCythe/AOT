3c60e460bd4dfd881f5d5f351f9d070fe9dd648f
Rize <dudersshark@.gmail.com>
2024-06-13 22:10:22 +0800

    W

--- AOTREVTEST.lua
+++ AOTREVTEST.lua
@@ -13,12 +13,6 @@ local Farm = true
 
 local workspace = game:GetService("Workspace")
 
-local function Anchored()
-    if Player.Character and Player.Character:FindFirstChild("HumanoidRootPart") then
-        Player.Character.HumanoidRootPart.Anchored = Farm
-    end
-end
-
 local function findNape(hitFolder)
     return hitFolder:FindFirstChild("Nape")
 end
@@ -151,7 +145,6 @@ end
 
 while Farm do
     pcall(function()
-        Anchored()
         Parry()
 
         local titansList = GetTitans()
